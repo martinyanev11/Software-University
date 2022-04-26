44151aad368996b5b15436a4a461f77a92616a3f
Martin Yanev <marrtin@abv.bg>
2022-04-26 17:56:17 +0300

    Nested Loops
    Programming Basics - C# - Nested Loops - Exercise. Console applications I coded.

---
+++ Programming Basics - C#/Nested Loops/Exercise/01. Number Pyramid/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _01._Number_Pyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int numberToPrint = 1;
+            bool donePrinting = false;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = -1; j < i; j++)
+                {
+                    Console.Write($"{numberToPrint} ");
+                    numberToPrint++;
+
+                    if (numberToPrint - 1 == n)
+                    {
+                        donePrinting = true;
+                        break;
+                    }
+                }
+                Console.WriteLine();
+                if (donePrinting)
+                {
+                    break;
+                }
+
+            }
+        }
+    }
+}
---
+++ Programming Basics - C#/Nested Loops/Exercise/02. Equal Sums Even Odd Position/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _02._Equal_Sums_Even_Odd_Position
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startNumber = int.Parse(Console.ReadLine());
+            int endNumber = int.Parse(Console.ReadLine());
+
+            for (int i = startNumber; i <= endNumber; i++)
+            {
+                int evenSum = 0;
+                int oddSum = 0;
+
+                int delimeter = 10;
+                int currentNumber = i;
+
+                for (int j = 1; j <= 6; j++) // 6 - because we know we have numbers with 6 digits.
+                {
+                    int currentLastDigit = currentNumber % delimeter;
+                    currentNumber /= delimeter;
+
+                    if (j % 2 == 0)
+                    {
+                        evenSum += currentLastDigit;
+                    }
+                    else
+                    {
+                        oddSum += currentLastDigit;
+                    }
+                }
+                if (evenSum == oddSum)
+                {
+                    Console.Write($"{i} ");
+                }
+            }
+        }
+    }
+}
