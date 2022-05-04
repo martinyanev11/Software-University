3014a12a65c40751a0e4673fd1bef2d747cef48b
Martin Yanev <marrtin@abv.bg>
2022-05-04 20:58:29 +0300

    Arrays
    Arrays - More Exercise - solved  Console Applications: 01. Encrypt, Sort and Print Array

---
+++ Programming Fundamentals - C#/Arrays/More Exercise/01. Encrypt, Sort and Print Array/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace _01._Encrypt__Sort_and_Print_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Reading input
+            int numberOfInputs = int.Parse(Console.ReadLine());
+
+            string[] names = new string[numberOfInputs];
+            int[] encrypted = new int[numberOfInputs];
+
+            for (int i = 0; i < numberOfInputs; i++)
+            {
+                names[i] = Console.ReadLine();
+            }
+
+            int counter = 0; // This counter keeps track of which index we should add "currNameSum" into the encrypted array
+            foreach (string name in names)
+            {
+                int currNameSum = 0;
+                bool isVowel = false;
+
+                for (int i = 0; i < name.Length; i++)
+                {
+                    // Check if the char on index i is a vowel or consonant
+                    if ("aeiouAEIOU".Contains(name[i].ToString()))
+                    {
+                        isVowel = true;
+                    }
+                    else
+                    {
+                        isVowel = false; // This will make sure it goes into the right if statement below (restarting the bool)
+                    }
+
+                    if (isVowel == true)
+                    {
+                        currNameSum += name[i] * name.Length;
+                    }
+                    else
+                    {
+                        currNameSum += name[i] / name.Length;
+                    }
+                }
+
+                encrypted[counter] = currNameSum;
+                counter++;
+            }
+
+            Array.Sort(encrypted);
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine($"{encrypted[i]}"); 
+            }
+        }
+    }
+}
---
+++ Programming Fundamentals - C#/Arrays/More Exercise/02. Pascal Triangle/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Pascal_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNumber = int.Parse(Console.ReadLine());
+            int[] pascalTriangleArray = new int[inputNumber];
+
+            bool isEven = false;
+            int printCounter = inputNumber - 1;
+
+            if (inputNumber % 2 == 0)
+            {
+                isEven = true;
+                printCounter--;
+            }
+
+            // Add the first number on middle index to be = 1
+            pascalTriangleArray[inputNumber / 2] = 1;
+
+            // Print the first line
+            Console.WriteLine(String.Join(" ", pascalTriangleArray.Where(x => x != 0)));
+
+            // Then if the array length is even we add another "1" on the middle index - 1
+            if (isEven == true)
+            {
+                pascalTriangleArray[(inputNumber / 2) - 1] = 1;
+            }
+            
+            Console.WriteLine(String.Join(" ", pascalTriangleArray.Where(x => x != 0)));
+
+            int leftCounter = printCounter / 2 - 1;
+            int rightCounter = printCounter / 2 + 1;
+
+            for (int i = 0; i < printCounter; i++)
+            {
+
+            }
+        }
+    }
+}
