379c8f3923c7b73fbe3589b0125306bfc245d6d5
Martin Yanev <marrtin@abv.bg>
2022-05-06 11:00:00 +0300

    Methods
    Methods - More Exercise. Programming Fundamentals - C# in SoftUni.

---
+++ Programming Fundamentals - C#/Methods/More Exercise/01. Data Types/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _01._Data_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "int":
+                    int intNumber = int.Parse(Console.ReadLine());
+                    int intResult = Multiply(intNumber);
+                    Console.WriteLine(intResult);
+                    break;
+                case "real":
+                    double doubleNumber = double.Parse(Console.ReadLine());
+                    double doubleResult = Multiply(doubleNumber);
+                    Console.WriteLine($"{doubleResult:f2}");
+                    break;
+                case "string":
+                    string @string = Console.ReadLine();
+                    Console.WriteLine($"${@string}$");
+                    break;
+            }
+        }
+
+        static int Multiply(int number)
+        {
+            return number * 2;
+        }
+
+        static double Multiply(double number)
+        {
+            return number * 1.5;
+        }
+    }
+}
