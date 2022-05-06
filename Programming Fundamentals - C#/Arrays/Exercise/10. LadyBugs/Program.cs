3e5880afb68c4d488a85b4a6ac692b0f9b289bfa
Martin Yanev <marrtin@abv.bg>
2022-05-06 12:03:22 +0300

    Arrays
    Arrays - Exercise / 10. LadyBugs Solved.

--- Programming Fundamentals - C#/Arrays/Exercise/10. LadyBugs/Program.cs
+++ Programming Fundamentals - C#/Arrays/Exercise/10. LadyBugs/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
 using System.Linq;
 
 namespace _10._LadyBugs
@@ -8,94 +11,128 @@ namespace _10._LadyBugs
         static void Main(string[] args)
         {
             int fieldSize = int.Parse(Console.ReadLine());
-            int[] ladybugPosittionsArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
-            int[] field = new int[fieldSize];
 
-            foreach (int ladybugPosition in ladybugPosittionsArray)
+            int[] lBugInitialPositionIndex = Console.ReadLine()      // get initial Bug positions
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] ladyBugField = new int[fieldSize];
+
+            for (int i = 0; i < ladyBugField.Length; i++)   //setting '1' on initial bug positions
             {
-                for (int i = 0; i < fieldSize; i++)
+                if (lBugInitialPositionIndex.Contains(i))
                 {
-                    if (i == ladybugPosition)
+                    ladyBugField[i] = 1;
+                }
+            }
+
+            string command = Console.ReadLine();
+
+            while (command != "end")
+            {
+                string[] rules = command
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                int initialPosition = int.Parse(rules[0]);
+                string direction = rules[1];
+                int countOfMoves = int.Parse(rules[2]);
+
+                int newPosition = 0;
+
+                if (countOfMoves < 0)
+                {
+                    if (direction == "left")
                     {
-                        field[i] = 1;
+                        direction = "right";
+                        countOfMoves = Math.Abs(countOfMoves);
+                    }
+                    else if (direction == "right")
+                    {
+                        direction = "left";
+                        countOfMoves = Math.Abs(countOfMoves);
+                    }
+                }
+
+                if (initialPosition < 0     //check if initial position is out of array or if initial position has NO bug
+                   || initialPosition > ladyBugField.Length - 1
+                   || ladyBugField[initialPosition] == 0)
+                {
+                    command = Console.ReadLine();
+                    continue;
+                }
+
+                if (countOfMoves == 0 && initialPosition >= 0 && initialPosition <= ladyBugField.Length - 1)
+                {
+                    if (ladyBugField[initialPosition] == 1)
+                    {
+                        ladyBugField[initialPosition] = 0;
                     }
 
                 }
-            }
-            bool isEnded = false;
 
-            while (isEnded == false)
-            {
-                string possitionChangeCommand = Console.ReadLine();
-                if (possitionChangeCommand == "end")
+                switch (direction)
                 {
-                    isEnded = true;
-                }
-                else
-                {
-                    // UNDERSTANDING THE COMMAND GIVEN
-                    string[] commandReading = possitionChangeCommand.Split(" ", StringSplitOptions.RemoveEmptyEntries);
-                    int ladybugStartingIndex = int.Parse(commandReading[0]);
-                    string ladybugMovementDirection = commandReading[1];
-                    int ladybugFlyLength = int.Parse(commandReading[2]);
 
-                    // EXECUTING THE COMMAND GIVEN
-                    // Check if the ladybug index exists in the array
-                    if (field[ladybugStartingIndex] >= 0 && field[ladybugStartingIndex] < fieldSize)
-                    {
-                        if (field[ladybugStartingIndex] == 1) // IF THERE IS LADYBUG IN INDEX
+                    case "right":
+
+                        newPosition = initialPosition + countOfMoves; //find new position index
+
+                        ladyBugField[initialPosition] = 0; //both cases initial index gets '0'
+
+                        if (newPosition > ladyBugField.Length - 1) //check if new position index is out of array
                         {
-                            field[ladybugStartingIndex] = 0; // IT FLIES AWAY
-
-                            // DETERMINE THE DIRECTION
-                            if (ladybugMovementDirection == "right")
-                            {
-                                if (ladybugStartingIndex + ladybugFlyLength < fieldSize) // Check if the bug lands within the array scope
-                                {
-                                    if (field[ladybugStartingIndex + ladybugFlyLength] != 1) // If no other ladybug on that index
-                                    {
-                                        field[ladybugStartingIndex + ladybugFlyLength] = 1; // Then it lands there
-                                    }
-                                    else
-                                    {
-                                        // Keep flying till it finds empty spot within the array
-                                        for (int i = ladybugStartingIndex; i < fieldSize - 1; i += ladybugFlyLength)
-                                        {
-                                            if (field[i + ladybugFlyLength] != 1)
-                                            {
-                                                field[i + ladybugFlyLength] = 1; // Found empty spot and landed
-                                                break;
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                            else if (ladybugMovementDirection == "left")
-                            {
-                                if (ladybugStartingIndex - ladybugFlyLength >= 0)
-                                {
-                                    if (field[ladybugStartingIndex - ladybugFlyLength] != 1)
-                                    {
-                                        field[ladybugStartingIndex - ladybugFlyLength] = 1;
-                                    }
-                                    else
-                                    {
-                                        for (int i = ladybugStartingIndex; i >= 0; i -= ladybugFlyLength)
-                                        {
-                                            if (field[i - ladybugFlyLength] != 1)
-                                            {
-                                                field[i - ladybugFlyLength] = 1;
-                                                break;
-                                            }
-                                        }
-                                    }
-                                }
-                            }
+                            ladyBugField[initialPosition] = 0;  //if it is out , cuurent position get '0'-LB flies out of the array
+                            break;
                         }
-                    }
+                        else
+                        {
+                            for (int i = newPosition; i < ladyBugField.Length; i += countOfMoves) // array goes till LB find free index, or goes out of tje field
+                            {
+                                if (ladyBugField[i] == 0) //if new position has no LB, current LB land here
+                                {
+                                    ladyBugField[i] = 1;
+                                    break;
+                                }
+                            }
+
+                        }
+                        break;
+
+                    case "left":
+
+                        newPosition = initialPosition - countOfMoves;
+
+
+                        ladyBugField[initialPosition] = 0; //in both cases initial position gets '0'
+                        if (newPosition < 0) //if new posiion is out of array initial index of LB gets '0'
+                        {
+                            ladyBugField[initialPosition] = 0;
+                            break;
+                        }
+
+                        else
+                        {
+                            for (int i = newPosition; i >= 0; i -= countOfMoves) //find free index or LB gets out of the array
+                            {
+                                if (ladyBugField[i] == 0)  //if index is free LB lands here
+                                {
+                                    ladyBugField[i] = 1;
+                                    break;
+                                }
+                            }
+
+                        }
+                        break;
+                    default:
+                        break;
                 }
+
+                command = Console.ReadLine();
             }
-            Console.WriteLine(string.Join(" ", field));
+            Console.WriteLine(string.Join(" ", ladyBugField));
+
         }
     }
 }
