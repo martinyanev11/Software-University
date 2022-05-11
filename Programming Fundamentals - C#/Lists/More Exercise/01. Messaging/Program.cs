ade5e7526bd3356b8dead0dcd9ef3bf682bb6599
Martin Yanev <marrtin@abv.bg>
2022-05-11 23:18:37 +0300

    More Exercise
    Programing Fundamentals - C#. Lists - More Exercise console applications 01. and 02.

---
+++ Programming Fundamentals - C#/Lists/More Exercise/01. Messaging/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Messaging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numberList = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            string sentence = Console.ReadLine();
+
+            List<int> summedNumbers = new List<int>();
+            foreach (int number in numberList)
+            {
+                int tempSum = 0;
+                int lastDigit = 0;
+                int currentNumber = number;
+
+                while (currentNumber > 0)
+                {
+                    lastDigit = currentNumber % 10;
+                    tempSum += lastDigit;
+                    currentNumber /= 10;
+                }
+
+                summedNumbers.Add(tempSum);
+            }
+
+            string outputMessage = string.Empty;
+            
+            foreach (int summedNumber in summedNumbers)
+            {
+                int currSumNum = summedNumber;
+                while (currSumNum > sentence.Length - 1)
+                {
+                    currSumNum -= sentence.Length;
+                }
+
+                char tempChar = sentence[currSumNum];
+                outputMessage += tempChar;
+                sentence = sentence.Remove(currSumNum, 1);    
+            }
+
+            Console.WriteLine(outputMessage);
+        }
+    }
+}
---
+++ Programming Fundamentals - C#/Lists/More Exercise/02. Car Race/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Car_Race
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> inputList = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            List<int> firstRacer = new List<int>();
+            List<int> secondRacer = new List<int>();
+
+            int finishLine = inputList.Count / 2 + 1;
+
+            for (int i = 0; i < finishLine - 1; i++)
+            {
+                firstRacer.Add(inputList[i]);
+            }
+            for (int i = finishLine; i < inputList.Count; i++)
+            {
+                secondRacer.Add(inputList[i]);
+            }
+
+            secondRacer.Reverse();
+
+            decimal firstRacerSum = 0;
+            decimal secondRacerSum = 0;
+
+            for (int i = 0; i < firstRacer.Count; i++)
+            {
+                if (firstRacer[i] == 0)
+                {
+                    firstRacerSum *= 0.8M;
+                }
+                firstRacerSum += firstRacer[i];
+            }
+
+            for (int i = 0; i < secondRacer.Count; i++)
+            {
+                if (secondRacer[i] == 0)
+                {
+                    secondRacerSum *= 0.8M;
+                }
+                secondRacerSum += secondRacer[i];
+            }
+
+            if (firstRacerSum < secondRacerSum)
+            {
+                Console.WriteLine($"The winner is left with total time: {firstRacerSum}");
+            }
+            else
+            {
+                Console.WriteLine($"The winner is right with total time: {secondRacerSum}");
+            }
+        }
+    }
+}
