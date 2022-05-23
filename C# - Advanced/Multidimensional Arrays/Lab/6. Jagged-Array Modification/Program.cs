1f93733d7ac2ddf31f545ebc4eeeaad6f274d4d5
Martin Yanev <marrtin@abv.bg>
2022-05-23 13:22:51 +0300

    Multidimensional Arrays
    Software University - C# Advanced - Multidimensional Arrays - Lab console applications 6 & 7 solved. Jagged Matrix

---
+++ C# - Advanced/Multidimensional Arrays/Lab/6. Jagged-Array Modification/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _6._Jagged_Array_Modification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Reading a jagged array from console
+            int rowCount = int.Parse(Console.ReadLine());
+            int[][] jaggedMatrix = new int[rowCount][];
+
+            for (int row = 0; row < jaggedMatrix.Length; row++)
+            {
+                int[] newRow = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                jaggedMatrix[row] = new int[newRow.Length];
+
+                for (int col = 0; col < jaggedMatrix[row].Length; col++)
+                {
+                    jaggedMatrix[row][col] = newRow[col];
+                }
+            }
+
+            string input;
+            while ((input = Console.ReadLine()) != "END")
+            {
+                // Executing commands
+                string[] cmdArgs = input.Split();
+                string commandType = cmdArgs[0];
+                int rowToChange = int.Parse(cmdArgs[1]);
+                int colToChange = int.Parse(cmdArgs[2]);
+                int value = int.Parse(cmdArgs[3]);
+
+                if (rowToChange >= 0 && rowToChange < jaggedMatrix.Length)
+                {
+                    if (colToChange >= 0 && colToChange < jaggedMatrix[rowToChange].Length)
+                    {
+                        if (commandType == "Add")
+                        {
+                            jaggedMatrix[rowToChange][colToChange] += value;
+                        }
+                        else if (commandType == "Subtract")
+                        {
+                            jaggedMatrix[rowToChange][colToChange] -= value;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid coordinates");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid coordinates");
+                }
+            }
+
+            // Printing the matrix on the console
+            foreach (int[] row in jaggedMatrix)
+            {
+                Console.WriteLine(string.Join(" ", row));
+            }
+            
+        }
+    }
+}
---
+++ C# - Advanced/Multidimensional Arrays/Lab/7. Pascal Triangle/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _7._Pascal_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rowsCount = int.Parse(Console.ReadLine());
+
+            long[][] jaggedMatrix = new long[rowsCount][];
+
+            for (int row = 0; row < jaggedMatrix.Length; row++)
+            {
+                jaggedMatrix[row] = new long[row + 1];
+                jaggedMatrix[row][0] = 1; // first element is always 1 on each row
+                jaggedMatrix[row][jaggedMatrix[row].Length - 1] = 1; // last element is always 1 as well
+
+                for (int col = 1; col < jaggedMatrix[row].Length - 1; col++)
+                {
+                    jaggedMatrix[row][col] = jaggedMatrix[row - 1][col - 1] + jaggedMatrix[row - 1][col];
+                }
+            }
+
+            // Printing the matrix on the console
+            foreach (long[] row in jaggedMatrix)
+            {
+                Console.WriteLine(string.Join(" ", row));
+            }
+        }
+    }
+}
