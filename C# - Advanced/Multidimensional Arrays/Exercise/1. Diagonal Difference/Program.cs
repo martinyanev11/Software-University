0636d71546df7eb378fe9448c61a877a17d0311c
Martin Yanev <marrtin@abv.bg>
2022-05-23 17:57:56 +0300

    Multidimensional Arrays
    Software University - C# Advanced course. Console applications for Exercise 1 & 2 solved.

---
+++ C# - Advanced/Multidimensional Arrays/Exercise/1. Diagonal Difference/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1._Diagonal_Difference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int squareMatrixSize = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[squareMatrixSize, squareMatrixSize];
+
+            for (int row = 0; row < squareMatrixSize; row++)
+            {
+                int[] rowInput = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int col = 0; col < rowInput.Length; col++)
+                {
+                    matrix[row, col] = rowInput[col];
+                }
+            }
+
+            // Summing the elements in primary diagonal
+            int primaryDiagonalSum = 0;
+            
+            for (int i = 0; i < squareMatrixSize; i++)
+            {
+                primaryDiagonalSum += matrix[i, i];
+            }
+
+            // Summing the elements in the secondary diagonal
+            int secondaryDiagonalSum = 0;
+            int currRow = 0;
+
+            for (int col = squareMatrixSize - 1; col >= 0; col--)
+            {
+                secondaryDiagonalSum += matrix[currRow, col];
+                currRow++;
+            }
+
+            int absoluteDifference = Math.Abs(primaryDiagonalSum - secondaryDiagonalSum);
+            Console.WriteLine(absoluteDifference);
+        }
+    }
+}
---
+++ C# - Advanced/Multidimensional Arrays/Exercise/2. Squares in Matrix/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2._Squares_in_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create matrix with given dimensions from the console
+            int[] matrixDimensions = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int rows = matrixDimensions[0];
+            int columns = matrixDimensions[1];
+
+            string[,] matrix = new string[rows, columns];
+
+            // Fill the matrix with info from the console
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string[] currRowInfo = Console.ReadLine().Split();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = currRowInfo[col];
+                }
+            }
+
+            // Find 2x2 matrix with same chars
+            int sameCharMatrixes2x2Found = 0;
+
+            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+                {
+                    if (matrix[row, col] == matrix[row, col + 1] && matrix[row, col] ==
+                        matrix[row + 1, col] && matrix[row, col] == matrix[row + 1, col + 1])
+                    {
+                        sameCharMatrixes2x2Found++;
+                    } 
+
+                }
+            }
+
+            Console.WriteLine(sameCharMatrixes2x2Found);
+
+        }
+    }
+}
