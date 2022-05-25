64c1b419b8cca25fee65eeef21954a16f93d4e88
Martin Yanev <marrtin@abv.bg>
2022-05-25 20:45:51 +0300

    Multidimensional Arrays
    Solved problem 9 and almost finished 10

---
+++ C# - Advanced/Multidimensional Arrays/Exercise/10. Radioactive Mutant Vampire/Program.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _10._Radioactive_Mutant_Vampire_Bunnies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initialize the playing field with it's properties
+            int[] dimensions = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int rows = dimensions[0];
+            int columns = dimensions[1];
+
+            char[,] field = new char[rows, columns];
+            int[] playerCoordinates = new int[2];
+            InitializeBunnyLair(field, playerCoordinates);
+
+            // Load a queue of commands to be executed
+            string cmdArgs = Console.ReadLine();
+            Queue<char> command = new Queue<char>(cmdArgs);
+
+            // Start game
+            bool steppedOnBunny = false;
+            for (int rounds = 0; rounds < command.Count; rounds++)
+            {
+                char nextCommand = command.Dequeue();
+
+                // Check if player remains in the lair after moving
+                if (PlayerMove(field, playerCoordinates, nextCommand)) // Game continues
+                {
+                    //Check if player stepped on buddy after his move
+                    if (SteppedOnBunnyCheck(field, playerCoordinates))
+                    {
+                        steppedOnBunny = true;
+                    }
+                    BunniesMultiplication(field);
+                    if (steppedOnBunny)
+                    {
+                        break;
+                    }
+                    // Check if bunny spawned on player location index after multiplication
+                    if (SteppedOnBunnyCheck(field, playerCoordinates))
+                    {
+                        steppedOnBunny = true;
+                        break;
+                    }
+                }
+                else // Player won
+                {
+                    PlayerWonMessage(field, playerCoordinates);
+                    break;
+                }
+            }
+
+            if (steppedOnBunny)
+            {
+                PlayerLostMessage(field, playerCoordinates);
+            }
+
+        }
+
+        private static void PlayerWonMessage(char[,] field, int[] playerCoordinates)
+        {
+            field[playerCoordinates[0], playerCoordinates[1]] = '.'; // replace 'P' with '.'
+
+            for (int row = 0; row < field.GetLength(0); row++)
+            {
+                for (int col = 0; col < field.GetLength(1); col++)
+                {
+                    Console.Write(field[row, col]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine($"won: {playerCoordinates[0]} {playerCoordinates[1]}");
+        }
+
+        private static void PlayerLostMessage(char[,] field, int[] playerCoordinates)
+        {
+            for (int row = 0; row < field.GetLength(0); row++)
+            {
+                for (int col = 0; col < field.GetLength(1); col++)
+                {
+                    Console.Write(field[row, col]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine($"dead: {playerCoordinates[0]} {playerCoordinates[1]}");
+        }
+
+        private static void InitializeBunnyLair(char[,] field, int[] playerCoordinates)
+        {
+            for (int row = 0; row < field.GetLength(0); row++)
+            {
+                string rowData = Console.ReadLine();
+                for (int col = 0; col < field.GetLength(1); col++)
+                {
+                    field[row, col] = rowData[col];
+                    if (rowData[col] == 'P')
+                    {
+                        playerCoordinates[0] = row;
+                        playerCoordinates[1] = col;
+                    }
+                }
+            }
+        }
+
+        private static void BunniesMultiplication(char[,] field)
+        {
+            // Find initial bunny coordinates
+            Queue<int[]> allBunnyCoordinates = new Queue<int[]>();
+            for (int row = 0; row < field.GetLength(0); row++)
+            {
+                for (int col = 0; col < field.GetLength(1); col++)
+                {
+                    if (field[row, col] == 'B')
+                    {
+                        allBunnyCoordinates.Enqueue(new int[] { row, col });
+                    }
+                }
+            }
+
+            // Multiply each bunny to all nearby legit indexes
+            for (int i = 0; i < allBunnyCoordinates.Count; i++)
+            {
+                int[] currentBunnyCoordinates = allBunnyCoordinates.Dequeue();
+                int bunnyRow = currentBunnyCoordinates[0];
+                int bunnyCol = currentBunnyCoordinates[1];
+
+                // Replace all indexes within the lair with 'B'
+                MultiplicationProcess(field, bunnyRow, bunnyCol);
+            }
+        }
+
+        private static void MultiplicationProcess(char[,] field, int row, int col)
+        {
+            if (row - 1 >= 0 && col - 1 >= 0) //#1 top left
+            {
+                field[row - 1, col - 1] = 'B';
+            }
+
+            if (col - 1 >= 0) //#2 left
+            {
+                field[row, col - 1] = 'B';
+            }
+
+            if (row + 1 < field.GetLength(0) && col - 1 >= 0) // #3 bottom left
+            {
+                field[row + 1, col - 1] = 'B';
+            }
+
+            if (row - 1 >= 0) //#4 top
+            {
+                field[row - 1, col] = 'B';
+            }
+
+            if (row + 1 < field.GetLength(0)) //#5 bottom
+            {
+                field[row + 1, col] = 'B';
+            }
+
+            if (row - 1 >= 0 && col + 1 < field.GetLength(1)) //#6 top right
+            {
+                field[row - 1, col + 1] = 'B';
+            }
+
+            if (col + 1 < field.GetLength(1)) //#7 right
+            {
+                field[row, col + 1] = 'B';
+            }
+
+            if (row + 1 < field.GetLength(0) && col + 1 < field.GetLength(1)) //#8 bottom right
+            {
+                field[row + 1, col + 1] = 'B';
+            }
+        }
+
+        private static bool SteppedOnBunnyCheck(char[,] field, int[] playerCoordinates)
+        {
+            if (field[playerCoordinates[0], playerCoordinates[1]] == 'B')
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private static bool PlayerMove(char[,] field, int[] playerCoordinates, char movementDirection)
+        {
+            switch (movementDirection)
+            {
+                case 'U': // up movement
+                    if (playerCoordinates[0] - 1 >= 0)
+                    {
+                        playerCoordinates[0] -= 1;
+                        return true; // player moves & game continues
+                    }
+                    else
+                    {
+                        return false; // player leaves the lair and wins the game
+                    }
+
+                case 'D': // down movement
+                    if (playerCoordinates[0] + 1 < field.GetLength(0))
+                    {
+                        playerCoordinates[0] += 1;
+                        return true; // player moves & game continues
+                    }
+                    else
+                    {
+                        return false; // player leaves the lair and wins the game
+                    }
+
+                case 'L': // left movement
+                    if (playerCoordinates[1] - 1 >= 0)
+                    {
+                        playerCoordinates[1] -= 1;
+                        return true; // player moves & game continues
+                    }
+                    else
+                    {
+                        return false; // player leaves the lair and wins the game
+                    }
+
+                case 'R': // right movement
+                    if (playerCoordinates[1] + 1 < field.GetLength(1))
+                    {
+                        playerCoordinates[1] += 1;
+                        return true; // player moves & game continues
+                    }
+                    else
+                    {
+                        return false; // player leaves the lair and wins the game
+                    }
+            }
+            return false;
+        }
+    }
+}
+
---
+++ C# - Advanced/Multidimensional Arrays/Exercise/9. Miner/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _9._Miner
+{
+    class Program
+    {
+        private static bool MoveCheckAndExecute(char[,] field, int[] playerCoordinates, string movementDirection)
+        {
+            switch (movementDirection)
+            {
+                case "up": // check if above row isn't out of the field boundry
+                    if (playerCoordinates[0] - 1 >= 0)
+                    {
+                        playerCoordinates[0] -= 1;
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+                case "down": // check if below row isn't out of the field boundry
+                    if (playerCoordinates[0] + 1 < field.GetLength(0))
+                    {
+                        playerCoordinates[0] += 1;
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+                case "left": // check if left column isn't out of the field boundry
+                    if (playerCoordinates[1] - 1 >= 0)
+                    {
+                        playerCoordinates[1] -= 1;
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+                case "right": // check if right column isn't out of the field boundry
+                    if (playerCoordinates[1] + 1 < field.GetLength(1))
+                    {
+                        playerCoordinates[1] += 1;
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+            }
+
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            int fieldSize = int.Parse(Console.ReadLine());
+            char[,] field = new char[fieldSize, fieldSize];
+
+            // Load the commands in queue
+            string[] cmdArgs = Console.ReadLine().Split();
+            Queue<string> commands = new Queue<string>();
+            for (int i = 0; i < cmdArgs.Length; i++)
+            {
+                commands.Enqueue(cmdArgs[i]);
+            }
+
+            // Initialize the play field with it's properties
+            int coalsRemaining = 0;
+            int[] playerCoordinates = new int[2];
+
+            for (int row = 0; row < fieldSize; row++)
+            {
+                char[] rowData = Console.ReadLine().Split().Select(char.Parse).ToArray();
+
+                for (int col = 0; col < fieldSize; col++)
+                {
+                    field[row, col] = rowData[col];
+                    if (rowData[col] == 'c')
+                    {
+                        coalsRemaining++;
+                    }
+                    if (rowData[col] == 's') // that is the starting possition of the player
+                    {
+                        playerCoordinates[0] = row;
+                        playerCoordinates[1] = col;
+                    }
+                }
+            }
+
+            // Execute commands accordingly
+            bool gameOver = false;
+            bool coalsCollected = false;
+            while (commands.Count > 0)
+            {
+                string nextCommand = commands.Peek();
+                if (MoveCheckAndExecute(field, playerCoordinates, nextCommand))
+                {
+                    if (field[playerCoordinates[0], playerCoordinates[1]] == 'e')
+                    {
+                        Console.WriteLine($"Game over! ({playerCoordinates[0]}, {playerCoordinates[1]})");
+                        gameOver = true;
+                        break;
+                    }
+                    else if (field[playerCoordinates[0], playerCoordinates[1]] == 'c')
+                    {
+                        field[playerCoordinates[0], playerCoordinates[1]] = '*';
+                        coalsRemaining--;
+                        if (coalsRemaining == 0)
+                        {
+                            Console.WriteLine($"You collected all coals! ({playerCoordinates[0]}, {playerCoordinates[1]})");
+                            coalsCollected = true;
+                            break;
+                        }
+                    }
+                }
+                commands.Dequeue();
+            }
+
+            if (gameOver == false && coalsCollected == false)
+            {
+                Console.WriteLine($"{coalsRemaining} coals left. ({playerCoordinates[0]}, {playerCoordinates[1]})");
+            }
+        }
+
+
+    }
+}
