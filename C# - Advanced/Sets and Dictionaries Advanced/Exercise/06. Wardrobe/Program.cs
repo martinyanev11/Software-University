6e7de4de24c5d2089f5b9ff6774e5e58970dae91
Martin Yanev <marrtin@abv.bg>
2022-05-28 13:19:17 +0300

    Sets and Dictionaries Advanced
    Sets and Dictionaries Advanced - Exercise - problem 06 fixed and solved.

---
+++ C# - Advanced/Sets and Dictionaries Advanced/Exercise/06. Wardrobe/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _06._Wardrobe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wardrobe = new Dictionary<string, Dictionary<string, int>>();
+            int inputLines = int.Parse(Console.ReadLine());
+
+            InputDataCollection(wardrobe, inputLines);
+
+            DictionaryPrinting(wardrobe);
+        }
+
+        private static void InputDataCollection(Dictionary<string, Dictionary<string, int>> wardrobe, int inputLines)
+        {
+            for (int i = 0; i < inputLines; i++)
+            {
+
+                string[] input = Console.ReadLine().Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+                string color = input[0];
+                string[] clothes = input[1].Split(",", StringSplitOptions.RemoveEmptyEntries);
+
+                if (!wardrobe.ContainsKey(color))
+                {
+                    wardrobe.Add(color, new Dictionary<string, int>());
+                }
+
+                foreach (string clothing in clothes)
+                {
+                    if (!wardrobe[color].ContainsKey(clothing))
+                    {
+                        wardrobe[color].Add(clothing, 0);
+                    }
+                    wardrobe[color][clothing]++;
+                }
+            }
+        }
+
+        private static void DictionaryPrinting(Dictionary<string, Dictionary<string, int>> wardrobe)
+        {
+            string[] searchData = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string colorToSearch = searchData[0];
+            string clothesToSearch = searchData[1];
+
+            foreach (var color in wardrobe)
+            {
+                Console.WriteLine($"{color.Key} clothes:");
+
+                foreach (var clothing in color.Value)
+                {
+                    if (color.Key == colorToSearch && clothing.Key == clothesToSearch)
+                    {
+                        Console.WriteLine($"* {clothing.Key} - {clothing.Value} (found!)");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"* {clothing.Key} - {clothing.Value}");
+                    }
+                    
+                }
+            }
+        }
+    }
+}
