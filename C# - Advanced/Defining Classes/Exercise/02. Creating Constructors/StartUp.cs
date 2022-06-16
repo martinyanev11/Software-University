5a951cdca678974d8878ddac6e800a81952f2b20
Martin Yanev <marrtin@abv.bg>
2022-06-16 20:12:09 +0300

    Defining Classes
    Software University - C# Advanced  course. Defining Classes - Exercise problems 1 & 2 solved.

---
+++ C# - Advanced/Defining Classes/Exercise/02. Creating Constructors/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            Person person1 = new Person(27);
+            Person person2 = new Person("Martin", 27);
+        }
+    }
+}
