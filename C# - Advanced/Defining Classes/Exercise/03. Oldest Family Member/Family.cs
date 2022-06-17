1c80364d60c6b7d4c12e73c2ef28f16b8009e9c5
Martin Yanev <marrtin@abv.bg>
2022-06-17 18:17:06 +0300

    Defining Classes
    C# Advanced. Defining Classes - Exercise problems 3 & 4 solved.

---
+++ C# - Advanced/Defining Classes/Exercise/03. Oldest Family Member/Family.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        public List<Person> FamilyMembers { get; set; }
+
+        public Family()
+        {
+            FamilyMembers = new List<Person>();
+        }
+
+        public void AddMember(Person member)
+        {
+            this.FamilyMembers.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            //int oldestAge = int.MinValue;
+            int oldestAge = this.FamilyMembers.Max(x => x.Age);
+            return this.FamilyMembers.First(x => x.Age == oldestAge);
+        }
+    }
+}
---
+++ C# - Advanced/Defining Classes/Exercise/04. Opinion Poll/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                string name = input[0];
+                int age = int.Parse(input[1]);
+                Person person = new Person(name, age);
+                people.Add(person);
+            }
+
+            
+            foreach (Person person in people.OrderBy(x => x.Name))
+            {
+                if (person.Age > 30)
+                {
+                    Console.WriteLine($"{person.Name} - {person.Age}");
+                }
+            }
+        }
+    }
+}
