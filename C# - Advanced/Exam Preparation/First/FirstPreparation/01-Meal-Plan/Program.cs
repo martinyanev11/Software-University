9abcc2b596e08850ba5fdabd1a92310efabffff6
Martin Yanev <marrtin@abv.bg>
2022-06-23 13:50:31 +0300

    Exam Preparation
    Solved the first problems for the exam preparation for C# Advanced course.

---
+++ C# - Advanced/Exam Preparation/First/FirstPreparation/01-Meal-Plan/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _01_Meal_Plan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] mealsInput = Console.ReadLine().Split();
+            int[] caloriesInput = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Queue<string> meals = new Queue<string>(mealsInput);
+            Stack<int> dailyCallories = new Stack<int>(caloriesInput);
+
+            Dictionary<string, int> mealCallories = new Dictionary<string, int>
+            {
+                { "salad", 350 },
+                { "soup", 490 },
+                { "pasta", 680 },
+                { "steak", 790 }
+            };
+
+            bool ateAllCallories = false;
+
+            while (meals.Count > 0 && dailyCallories.Count > 0)
+            {
+                string currentMeal = meals.Peek();
+                int currentDailyCalories = dailyCallories.Pop();
+
+                int currentMealCallories = mealCallories[currentMeal];
+
+                if (currentDailyCalories - currentMealCallories > 0)
+                {
+                    currentDailyCalories -= currentMealCallories;
+                    
+                    dailyCallories.Push(currentDailyCalories); // push back the left calories
+                    meals.Dequeue(); // the meal is eaten, remove it from the queue
+                }
+                else if (currentDailyCalories - currentMealCallories == 0)
+                {
+                    meals.Dequeue(); // The currentDailyCalories has been "Poped", we just dequeue the meal
+                }
+                else if (currentDailyCalories - currentMealCallories < 0)
+                {
+                    int leftOverCalories = currentMealCallories - currentDailyCalories;
+                    if (dailyCallories.Count > 0)
+                    {
+                        int nextDayDailyCalories = dailyCallories.Pop();
+
+                        // I assume due to constains teling me I don't need recursion,
+                        // the leftover is always less cal than the next day calories
+                        nextDayDailyCalories -= leftOverCalories;
+
+                        dailyCallories.Push(nextDayDailyCalories); // push back the nextdayCalories
+                        meals.Dequeue(); // Remove the eaten meal
+                    }
+                    else
+                    {
+                        meals.Dequeue(); // Remove the meal even if not fully eaten according to the task
+
+                        Console.WriteLine($"John ate enough, he had {mealsInput.Length - meals.Count} meals.");
+                        Console.WriteLine($"Meals left: {string.Join (", ", meals)}.");
+                        ateAllCallories = true;
+                        break;
+                    }
+                }
+            }
+
+            if (!ateAllCallories)
+            {
+                Console.WriteLine($"John had {mealsInput.Length} meals.");
+                Console.WriteLine($"For the next few days, he can eat {string.Join(", ", dailyCallories)} calories.");
+            }
+        }
+    }
+}
---
+++ C# - Advanced/Exam Preparation/First/FirstPreparation/02-Beaver-At-Work/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _02_Beaver_At_Work
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine()); // size of the pond
+
+            char[,] matrix = new char[size, size];
+            int branchesTotalCount = 0;
+            int[] beaverCoordinates = new int[2];
+
+            Stack<char> collectedBranches = new Stack<char>();
+
+            for (int row = 0; row < size; row++)
+            {
+                for (int col = 0; col < size; col++)
+                {
+                    char[] input = Console.ReadLine().Split().Select(char.Parse).ToArray();
+                    matrix[row, col] = input[col];
+
+                    if (char.IsLower(input[col])) // count the number of branches
+                    {
+                        branchesTotalCount++;
+                    }
+                    if (input[col] == 'B') // get the initial beaver possition
+                    {
+                        beaverCoordinates[0] = row;
+                        beaverCoordinates[1] = col;
+                    }
+                }
+            }
+
+            string command;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                BeaverMoves(matrix, command, beaverCoordinates, collectedBranches, branchesTotalCount);
+            }
+        }
+
+        private static void BeaverMoves(char[,] matrix, string command, int[] beaverCoordinates, 
+            Stack<char> collectedBranches, int branchesTotalCount)
+        {
+            switch (command)
+            {
+                case "up":
+                    if (beaverCoordinates[0] - 1 >= 0)
+                    {
+                        if (matrix[beaverCoordinates[0] - 1, beaverCoordinates[1]] == '-')
+                        {
+                            matrix[beaverCoordinates[0] - 1, beaverCoordinates[1]] = 'B';
+                            matrix[beaverCoordinates[0], beaverCoordinates[1]] = '-';
+
+                            beaverCoordinates[0] -= 1;
+                        }
+                        else if (char.IsLower(matrix[beaverCoordinates[0] - 1, beaverCoordinates[1]]))
+                        {
+                            collectedBranches.Push(matrix[beaverCoordinates[0] - 1, beaverCoordinates[1]]);
+                            branchesTotalCount--;
+
+                            matrix[beaverCoordinates[0] - 1, beaverCoordinates[1]] = 'B';
+                            matrix[beaverCoordinates[0], beaverCoordinates[1]] = '-';
+
+                            beaverCoordinates[0] -= 1;
+                        }
+                        else if (matrix[beaverCoordinates[0] - 1, beaverCoordinates[1]] == 'F')
+                        {
+                            BeaverSwims(matrix, command, beaverCoordinates, collectedBranches, branchesTotalCount);
+                        }
+                    }
+                    break;
+
+                case "down":
+                    break;
+
+                case "left":
+                    break;
+
+                case "right":
+                    break;
+            }
+        }
+
+        private static void BeaverSwims(char[,] matrix, string command, int[] beaverCoordinates, 
+            Stack<char> collectedBranches, int branchesTotalCount)
+        {
+            
+        }
+    }
+}
