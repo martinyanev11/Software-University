f162d9e97e73c0dc0e2be15149b9b5c16072f28e
Martin Yanev <marrtin@abv.bg>
2022-06-27 21:43:56 +0300

    Generics
    C# Advanced - Generics - Exercise problems 1 & 2 solved.

---
+++ C# - Advanced/Generics/Exercise/Box/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Box
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Box<string>> list = new List<Box<string>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                Box<string> box = new Box<string>(input);
+                list.Add(box);
+            }
+
+            int[] indexes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int indexOne = indexes[0];
+            int indexTwo = indexes[1];
+        }
+
+
+    }
+}
