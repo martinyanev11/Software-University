739f2cc0330f94da7725fb66a78c3c3fb5efe3ce
Martin Yanev <marrtin@abv.bg>
2022-06-28 18:26:04 +0300

    Generics
    C# Advanced - Generics - Exercise problems 3 & 4 solved.

---
+++ C# - Advanced/Generics/Exercise/Swap/Box.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Swap
+{
+    public class Box<T>
+    {
+        public Box(T element)
+        {
+            Element = element;
+        }
+
+        public Box(List<T> list)
+        {
+            List = list;
+        }
+
+        public T Element { get; set; }
+        public List<T> List { get; set; }
+
+        public void Swap(List<T> list, int indexOne, int indexTwo)
+        {
+            T tempData = list[indexOne];
+            list[indexOne] = list[indexTwo];
+            list[indexTwo] = tempData;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            foreach (T item in List)
+            {
+                sb.AppendLine($"{item.GetType()}: {item}");
+            }
+            return sb.ToString();
+        }
+
+    }
+}
---
+++ C# - Advanced/Generics/Exercise/Swap/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Swap
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var list = new List<string>();
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                list.Add(input);
+            }
+
+            int[] indexes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int indexOne = indexes[0];
+            int indexTwo = indexes[1];
+
+            var box = new Box<string>(list);
+            box.Swap(list, indexOne, indexTwo);
+            Console.WriteLine(box);
+        }
+    }
+}
