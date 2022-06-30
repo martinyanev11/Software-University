34537c8a22493b81b3467c76db8931410646c167
Martin Yanev <marrtin@abv.bg>
2022-06-30 21:51:23 +0300

    Iterators and Comparators
    C# Advanced - Iterators and Comparators - Lab, problem 1 solved.

---
+++ C# - Advanced/Iterators and Comparators/Lab/IteratorsAndComparators/Library.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace IteratorsAndComparators
+{
+    public class Library
+    {
+        public Library(params Book[] books)
+        {
+            this.books = books.ToList();
+        }
+
+        private List<Book> books { get; set; }
+    }
+}
