e97c0c6de7b4fdd46269ed1edbf67168cd221695
Martin Yanev <marrtin@abv.bg>
2022-07-02 11:59:23 +0300

    Inheritance
    C# OOP course @ Software University.
    Inheritance - Lab problems solved.

---
+++ C# - OOP/Inheritance/Lab/CustomRandomList/RandomList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        public string RandomString(List<string> list)
+        {
+            Random random = new Random();
+            int indexToRemove = random.Next(0, list.Count);
+
+            string removedString = list[indexToRemove];
+            list.RemoveAt(indexToRemove);
+            return removedString;
+        }
+    }
+}
---
+++ C# - OOP/Inheritance/Lab/Farm/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Farm
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            // Problem 01. Single Inheritance:
+            //Dog dog = new Dog();
+            //dog.Eat();
+            //dog.Bark();
+
+            // Problem 02. Multiple Inheritance:
+            //Puppy puppy = new Puppy();
+            //puppy.Eat();
+            //puppy.Bark();
+            //puppy.Weep();
+
+            // Problem 03. Hierarchical Inheritance:
+            Dog dog = new Dog();
+            dog.Eat();
+            dog.Bark();
+
+            Cat cat = new Cat();
+            cat.Eat();
+            cat.Meow();
+        }
+    }
+}
