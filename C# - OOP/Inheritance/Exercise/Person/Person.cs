a123f24d4647d46f29b052fbb2164d69e415afa6
Martin Yanev <marrtin@abv.bg>
2022-07-03 17:07:16 +0300

    Inheritance
    C# OOP - Inheritance - Exercise problems 1,2 and 3 solved.

---
+++ C# - OOP/Inheritance/Exercise/Person/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public int Age { get; set; }
+
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder strBuilder = new StringBuilder();
+            strBuilder.Append(String.Format($"Name: {this.Name}, Age: {this.Age}"));
+
+            return strBuilder.ToString();
+        }
+    }
+}
