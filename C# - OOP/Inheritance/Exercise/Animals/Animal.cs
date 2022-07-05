3f014f91e0cdae6b0601fdc6397185daada823ef
Martin Yanev <marrtin@abv.bg>
2022-07-05 17:47:25 +0300

    Inheritance
    C# OOP - Inheritance - Exercise problem Animals solved.

--- C# - OOP/Inheritance/Exercise/Animals/Animal.cs
+++ C# - OOP/Inheritance/Exercise/Animals/Animal.cs
@@ -4,11 +4,78 @@ using System.Text;
 
 namespace Animals
 {
-    public class Animal
+    public abstract class Animal
     {
-        public virtual string ProduceSound()
+        private string name;
+        private int age;
+        private string gender;
+
+        public Animal(string name, int age, string gender)
         {
-            throw new NotImplementedException();
+            this.Name = name;
+            this.Age = age;
+            this.Gender = gender;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("Invalid input!");
+                }
+                else
+                {
+                    name = value;
+                }
+
+            }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new Exception("Invalid input!");
+                }
+                else
+                {
+                    age = value;
+                }
+
+            }
+        }
+
+        public string Gender
+        {
+            get { return gender; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("Invalid input!");
+                }
+                else
+                {
+                    gender = value;
+                }
+            }
+        }
+
+        public abstract string ProduceSound();
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(this.GetType().Name);
+            sb.AppendLine($"{this.name} {this.Age} {this.Gender}");
+            sb.Append(this.ProduceSound());
+            return sb.ToString();
         }
     }
 }
--- C# - OOP/Inheritance/Exercise/Animals/StartUp.cs
+++ C# - OOP/Inheritance/Exercise/Animals/StartUp.cs
@@ -13,6 +13,7 @@ namespace Animals
             while ((command = Console.ReadLine()) != "Beast!")
             {
                 string[] cmdArgs = Console.ReadLine().Split();
+
                 string name = cmdArgs[0];
                 int age = int.Parse(cmdArgs[1]);
                 string gender = cmdArgs[2];
@@ -20,20 +21,35 @@ namespace Animals
                 switch (command)
                 {
                     case "Dog":
+                        Dog dog = new Dog(name, age, gender);
+                        animalsList.Add(dog);
                         break;
 
                     case "Cat":
+                        Cat cat = new Cat(name, age, gender);
+                        animalsList.Add(cat);
                         break;
 
                     case "Frog":
+                        Frog frog = new Frog(name, age, gender);
+                        animalsList.Add(frog);
                         break;
 
                     case "Kittens":
+                        Kitten kitten = new Kitten(name, age);
+                        animalsList.Add(kitten);
                         break;
 
                     case "Tomcat":
+                        Tomcat tomcat = new Tomcat(name, age);
+                        animalsList.Add(tomcat);
                         break;
                 }
+            }
+
+            foreach (var animal in animalsList)
+            {
+                Console.WriteLine(animal);
             }
         }
     }
