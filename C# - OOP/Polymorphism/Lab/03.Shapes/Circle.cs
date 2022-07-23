330ee6b19ea525be6a0a3fa8276b2c0c8dddce5d
Martin Yanev <marrtin@abv.bg>
2022-07-23 13:22:07 +0300

    Polymorphism
    C# OOP course @ Software University. Polymorphism - Lab problems 1 - 3 solved.

---
+++ C# - OOP/Polymorphism/Lab/03.Shapes/Circle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Circle : Shape
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            this.Radius = radius;
+        }
+
+        public double Radius
+        {
+            get { return this.radius; }
+            private set { this.radius = value; }
+        }
+
+        public override double CalculateArea()
+        {
+            return Math.PI * this.Radius * this.Radius;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * Math.PI * this.Radius;
+        }
+
+        public override string Draw()
+        {
+            return "Drawing Circle";
+        }
+    }
+}
---
+++ C# - OOP/Polymorphism/Lab/03.Shapes/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            //Rectangle rectangle = new Rectangle(5, 5);
+
+            //Console.WriteLine(rectangle.CalculatePerimeter());
+            //Console.WriteLine(rectangle.CalculateArea());
+
+            //Console.WriteLine(rectangle.Draw());
+
+
+            //Circle circle = new Circle(5);
+            //Console.WriteLine(circle.CalculatePerimeter());
+            //Console.WriteLine(circle.CalculateArea());
+
+            //Console.WriteLine(circle.Draw());
+        }
+    }
+}
